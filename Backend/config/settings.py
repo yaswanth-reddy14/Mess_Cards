d09452ae098e5b5776f139baebac20f27d90a118
Dondeti Yaswanth Reddy <reddyyaswanth1559@gmail.com>
2026-02-03 09:14:07 +0530

    Email OTP registration + migrations

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -72,6 +72,18 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "https://mess-cards-xyrg.vercel.app",
